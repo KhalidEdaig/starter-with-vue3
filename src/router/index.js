c874719d24e213960ea273380a44e662972b08c1
EDAIG KHALID <edaigkhalid@gmail.com>
2022-03-15 12:33:40 +0100

    feat: structure the project

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,41 @@ import Home from '/src/views/Home.vue'
 const routes = [
     {
         path: '/',
-        name: 'Home',
-        component: Home,
+        redirect: '/dashboard',
+        //component: DefaultLayout,
+        meta: { requiresAuth: true },
+        children: [
+            //{ path: '/dashboard', name: 'Dashboard', component: Dashboard },
+        ],
+    },
+    // {
+    //   path: "/",
+    //   name: 'Home',
+    //   component: Home
+    // },
+    {
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        //component: AuthLayout,
+        meta: { isGuest: true },
+        children: [
+            // {
+            //     path: '/login',
+            //     name: 'Login',
+            //     component: Login,
+            // },
+            // {
+            //     path: '/register',
+            //     name: 'Register',
+            //     component: Register,
+            // },
+        ],
+    },
+    {
+        path: '/404',
+        name: 'NotFound',
+        // component: NotFound,
     },
 ]
 
@@ -14,4 +47,15 @@ const router = createRouter({
     routes,
 })
 
+router.beforeEach((to, from, next) => {
+    let user = JSON.parse(localStorage.getItem('user'))
+
+    if (to.meta.requiresAuth && !user) {
+        next({ name: 'Login' })
+    } else if (user && to.meta.isGuest) {
+        next({ name: 'Dashboard' })
+    } else {
+        next()
+    }
+})
 export default router
